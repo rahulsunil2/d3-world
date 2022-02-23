e894ae41148744b2a6a0f3682c85c4c75df8ae5d
Rahul Sunil <rahulsunil2@gmail.com>
2022-02-24 01:56:14 +0530

    Splitted scripts

---
+++ scriptScatter.js
@@ -0,0 +1,89 @@
+const projectName = "bar-chart";
+
+var w = 800,
+	h = 400,
+	barWidth = 2,
+	padding = 60,
+	svg,
+	dataset;
+
+document.addEventListener("DOMContentLoaded", function () {
+	fetch(
+		"https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			dataset = data.data;
+			barWidth = (w - 2 * padding) / dataset.length;
+
+			initD3();
+		});
+});
+
+function initD3() {
+	svg = d3.select(".chart").append("svg").attr("width", w).attr("height", h);
+
+	const xScale = d3
+		.scaleTime()
+		.domain([
+			d3.min(dataset, (d) => new Date(d[0])),
+			d3.max(dataset, (d) => new Date(d[0])),
+		])
+		.range([padding, w - padding]);
+
+	const yScale = d3
+		.scaleLinear()
+		.domain([d3.max(dataset, (d) => d[1]), 0])
+		.range([padding, h - padding]);
+
+	const xAxis = d3.axisBottom(xScale).ticks(10);
+	const yAxis = d3.axisLeft(yScale).ticks(10);
+
+	const toolTip = d3
+		.select(".chart")
+		.append("div")
+		.attr("id", "tooltip")
+		.attr("style", "visibility: hidden;");
+
+	svg
+		.append("g")
+		.attr("id", "x-axis")
+		.attr("transform", "translate(0," + (h - padding) + ")")
+		.call(xAxis);
+	svg
+		.append("g")
+		.attr("id", "y-axis")
+		.attr("transform", "translate(" + padding + ", 0)")
+		.call(yAxis);
+
+	svg
+		.selectAll("rect")
+		.data(dataset)
+		.enter()
+		.append("rect")
+		.attr("x", (d, i) => xScale(new Date(d[0])))
+		.attr("width", barWidth)
+		.attr("y", (d, i) => yScale(d[1]))
+		.attr("height", (d, i) => h - padding - yScale(d[1]))
+		.attr("fill", "navy")
+		.attr("class", "bar")
+		.attr("data-date", (d) => d[0])
+		.attr("data-gdp", (d) => d[1])
+		.on("mouseover", function (e, d) {
+			var posX = e.pageX - 250;
+			var posY = e.pageY - 300;
+			toolTip
+				.attr(
+					"style",
+					"left:" + posX + "px;top:" + posY + "px; visibility: visible;"
+				)
+				.attr("data-date", d[0])
+				.html("<strong>" + d[1] + "</strong>");
+
+			d3.select(this).style("fill", "#ff33ff");
+		})
+		.on("mouseout", function (d) {
+			d3.select(this).style("fill", "navy");
+			toolTip.attr("style", "visibility: hidden;");
+		});
+}
