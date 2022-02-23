db9981d3e905f8b4b9b657c89767806bf558754b
Rahul Sunil <rahulsunil2@gmail.com>
2022-02-24 01:46:18 +0530

    yScale Set

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const projectName = "bar-chart";
 
 var w = 800,
 	h = 400,
-	barWidth = w / 275,
-	padding = 0,
+	barWidth = 2,
+	padding = 60,
 	svg,
 	dataset;
 
@@ -14,6 +14,8 @@ document.addEventListener("DOMContentLoaded", function () {
 		.then((response) => response.json())
 		.then((data) => {
 			dataset = data.data;
+			barWidth = (w - 2 * padding) / dataset.length;
+
 			initD3();
 		});
 });
@@ -21,23 +23,39 @@ document.addEventListener("DOMContentLoaded", function () {
 function initD3() {
 	svg = d3.select(".chart").append("svg").attr("width", w).attr("height", h);
 
-	const xScale = d3.scaleLinear().domain([0, dataset.length]).range([0, w]);
+	const xScale = d3
+		.scaleTime()
+		.domain([
+			d3.min(dataset, (d) => new Date(d[0])),
+			d3.max(dataset, (d) => new Date(d[0])),
+		])
+		.range([padding, w - padding]);
 
 	const yScale = d3
 		.scaleLinear()
-		.domain([0, d3.max(dataset, (d) => d[1])])
-		.range([0, h]);
+		.domain([d3.max(dataset, (d) => d[1]), 0])
+		.range([padding, h - padding]);
 
-	dataset.forEach((ele) =>
-		console.log(
-			ele,
-			yScale(ele[1]),
-			d3.max(dataset, (d) => d[1])
-		)
+	// console.log(
+	// 	d3.max(dataset, (d) => d[1]),
+	// 	yScale(0),
+	// 	xScale(0)
+	// );
+	console.log(
+		d3.min(dataset, (d) => new Date(d[0])),
+		d3.max(dataset, (d) => new Date(d[0])),
+		d3.min(dataset, (d) => d[0]),
+		xScale(new Date(d3.min(dataset, (d) => d[0])))
 	);
 
-	const xAxis = d3.axisBottom(xScale);
-	const yAxis = d3.axisLeft(yScale);
+	const xAxis = d3.axisBottom(xScale).ticks(10);
+	const yAxis = d3.axisLeft(yScale).ticks(10);
+
+	const toolTip = d3
+		.select(".chart")
+		.append("div")
+		.attr("id", "tooltip")
+		.attr("style", "visibility: hidden;");
 
 	svg
 		.append("g")
@@ -55,15 +73,30 @@ function initD3() {
 		.data(dataset)
 		.enter()
 		.append("rect")
-		.attr("x", (d, i) => i * 3)
-		.attr("y", (d, i) => h - yScale(d[1]))
-		.attr("width", 2)
-		.attr("height", (d, i) => yScale(d[1]))
+		.attr("x", (d, i) => padding + i * barWidth)
+		.attr("width", barWidth)
+		.attr("y", (d, i) => yScale(d[1]))
+		.attr("height", (d, i) => h - padding - yScale(d[1]))
+		// .attr("fake", (d, i) => console.log(d[1], yScale(d[1]), h - yScale(d[1])))
 		.attr("fill", "navy")
 		.attr("class", "bar")
 		.attr("data-date", (d) => d[0])
 		.attr("data-gdp", (d) => d[1])
-		.append("title")
-		.attr("id", "tooltip")
-		.text((d) => d[0]);
+		.on("mouseover", function (e, d) {
+			var posX = e.pageX - 250;
+			var posY = e.pageY - 300;
+			toolTip
+				.attr(
+					"style",
+					"left:" + posX + "px;top:" + posY + "px; visibility: visible;"
+				)
+				.attr("data-date", d[0])
+				.html("<strong>" + d[1] + "</strong>");
+
+			d3.select(this).style("fill", "#ff33ff");
+		})
+		.on("mouseout", function (d) {
+			d3.select(this).style("fill", "navy");
+			toolTip.attr("style", "visibility: hidden;");
+		});
 }
